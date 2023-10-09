24fadfe3c02f793c00a8a2ca0786b5d9d858fce8
vitalyvitmens <otm_@tut.by>
2023-10-09 17:16:00 +0300

    add file .env

--- backend/app.js
+++ backend/app.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
 const cookieParser = require('cookie-parser')
@@ -8,13 +9,9 @@ const app = express()
 app.use(cookieParser())
 app.use(express.json())
 
-mongoose
-	.connect(
-		'mongodb+srv://test:test123@cluster0.zgcseqm.mongodb.net/news-blog?retryWrites=true&w=majority'
-	)
-	.then(() => {
-		app.listen(port, () => {
-			console.log(`http://localhost:${port}/`)
-			console.log(`Server has been started on port ${port}...`)
-		})
+mongoose.connect(process.env.MONGODB_CONNECTION_STRING).then(() => {
+	app.listen(port, () => {
+		console.log(`http://localhost:${port}/`)
+		console.log(`Server has been started on port ${port}...`)
 	})
+})
