60a44fe69d16073116dc00a47b8e8b5be48bd4e2
vitalyvitmens <otm_@tut.by>
2023-10-17 16:35:32 +0300

    add views count

--- backend/controllers/post.js
+++ backend/controllers/post.js
@@ -2,61 +2,67 @@ const Post = require('../models/Post')
 
 // add
 async function addPost(post) {
-    const newPost = await Post.create(post);
+	const newPost = await Post.create(post)
 
-    await newPost.populate({
-        path: 'comments',
-        populate: 'author'
-    })
+	await newPost.populate({
+		path: 'comments',
+		populate: 'author',
+	})
 
-    return newPost
+	return newPost
 }
 
 // edit
 async function editPost(id, post) {
-    const newPost = await Post.findByIdAndUpdate(id, post, { returnDocument: 'after' })
+	const newPost = await Post.findByIdAndUpdate(id, post, {
+		returnDocument: 'after',
+	})
 
-    await newPost.populate({
-        path: 'comments',
-        populate: 'author'
-    })
+	await newPost.populate({
+		path: 'comments',
+		populate: 'author',
+	})
 
-    return newPost;
+	return newPost
 }
 
 // delete
 function deletePost(id) {
-    return Post.deleteOne({ _id: id })
+	return Post.deleteOne({ _id: id })
 }
 
 // get list with search and pagination
 async function getPosts(search = '', limit = 9, page = 1) {
-    const [posts, count] = await Promise.all([
-        Post.find({ title: { $regex: search, $options: 'i' } })
-            .limit(limit)
-            .skip((page - 1) * limit)
-            .sort({ createdAt: -1 }),
-        Post.countDocuments({ title: { $regex: search, $options: 'i' } })
-    ])
+	const [posts, count] = await Promise.all([
+		Post.find({ title: { $regex: search, $options: 'i' } })
+			.limit(limit)
+			.skip((page - 1) * limit)
+			.sort({ createdAt: -1 }),
+		Post.countDocuments({ title: { $regex: search, $options: 'i' } }),
+	])
 
-    return {
-        posts,
-        lastPage: Math.ceil(count / limit)
-    }
+	return {
+		posts,
+		lastPage: Math.ceil(count / limit),
+	}
 }
 
 // get item
-function getPost(id) {
-    return Post.findById(id).populate({
-        path: 'comments',
-        populate: 'author'
-    });
+async function getPost(id) {
+	const post = await Post.findById(id).populate({
+		path: 'comments',
+		populate: 'author',
+	})
+
+	post.views = post.views + 1
+	post.save()
+	return post
 }
 
 module.exports = {
-    addPost,
-    editPost,
-    deletePost,
-    getPosts,
-    getPost
+	addPost,
+	editPost,
+	deletePost,
+	getPosts,
+	getPost,
 }
